d0af710a98a7de6490f4958131b60ee3cbb091bb
Ahmad Youssef <ahmad.youssef@superhog.com>
2024-03-06 17:34:30 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { CartComponent } from './cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent, title: 'Home - Robots Shop'},
+  {path: 'catalog', component: CatalogComponent, title: 'Catalog - Robots Shop'},
+  {path: 'cart', component: CartComponent, title: 'Cart - Robots Shop'},
+  {path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { IProduct } from './product.model';
-import { CartService } from '../cart.service';
+import { CartService } from '../cart/cart.service';
 import { ProductService } from './product.service';
 import { subscribeOn } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'bot-catalog',
@@ -15,7 +16,9 @@ export class CatalogComponent {
 
   constructor(
     private cartSvc: CartService,
-    private productSvc: ProductService
+    private productSvc: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
   ) {}
 
   // Calling a method from products and subscribe to it to fetch the return data
@@ -23,13 +26,20 @@ export class CatalogComponent {
     this.productSvc.getProducts().subscribe((products) => {
       this.products = products;
     });
+    this.route.queryParams.subscribe((params)=>{
+      
+    console.log("*********trying to set *********"+params['filter']);
+      this.filter = params['filter']??'';
+    });
   }
 
   addToCart(product: IProduct) {
     this.cartSvc.add(product);
+    this.router.navigate(['/cart']);
   }
 
   getFiltererdProducts() {
+
     return this.filter === ''
       ? this.products
       : this.products.filter(
